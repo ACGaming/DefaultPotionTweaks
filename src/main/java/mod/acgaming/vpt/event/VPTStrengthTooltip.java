c2351d050d1ca4c6e1f236468eaab8d44a624a0b
ACGaming <4818419+ACGaming@users.noreply.github.com>
2025-02-03 11:49:04 +0100

    Improve strength tooltip injection

--- src/main/java/mod/acgaming/vpt/event/VPTStrengthTooltip.java
+++ src/main/java/mod/acgaming/vpt/event/VPTStrengthTooltip.java
@@ -31,17 +31,23 @@ public class VPTStrengthTooltip
                 {
                     if (effect.getPotion() == MobEffects.STRENGTH)
                     {
-                        int level = effect.getAmplifier() + 1;
-                        double factor = level * (VPTConfig.strengthAttackDamageFactor - 1);
-                        if (factor > 0.0D)
+                        for (int i = 0; i < event.getToolTip().size(); i++)
                         {
-                            event.getToolTip().add(TextFormatting.BLUE + I18n.translateToLocalFormatted("attribute.modifier.plus." + 0, ItemStack.DECIMALFORMAT.format(factor * 100.0D) + "%", I18n.translateToLocal("attribute.name.generic.attackDamage")));
+                            if (event.getToolTip().get(i).contains(I18n.translateToLocal("potion.whenDrank")))
+                            {
+                                int level = effect.getAmplifier() + 1;
+                                double factor = level * (VPTConfig.strengthAttackDamageFactor - 1);
+                                if (factor > 0.0D)
+                                {
+                                    event.getToolTip().add(i + 1, TextFormatting.BLUE + I18n.translateToLocalFormatted("attribute.modifier.plus." + 0, ItemStack.DECIMALFORMAT.format(factor * 100.0D) + "%", I18n.translateToLocal("attribute.name.generic.attackDamage")));
+                                }
+                                else if (factor < 0.0D)
+                                {
+                                    event.getToolTip().add(i + 1, TextFormatting.RED + I18n.translateToLocalFormatted("attribute.modifier.take." + 0, ItemStack.DECIMALFORMAT.format(factor * -100.0D) + "%", I18n.translateToLocal("attribute.name.generic.attackDamage")));
+                                }
+                                return;
+                            }
                         }
-                        else if (factor < 0.0D)
-                        {
-                            event.getToolTip().add(TextFormatting.RED + I18n.translateToLocalFormatted("attribute.modifier.take." + 0, ItemStack.DECIMALFORMAT.format(factor * -100.0D) + "%", I18n.translateToLocal("attribute.name.generic.attackDamage")));
-                        }
-                        break;
                     }
                 }
             }
